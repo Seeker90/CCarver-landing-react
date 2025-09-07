b742ea18a57ae2c33299ce21318e01ee633a246d
Seeker90 <chris.carver90@icloud.com>
2025-09-07 20:33:26 +0000

    first commit

---
+++ src/js/components/card.jsx
@@ -0,0 +1,17 @@
+import cardImage from"../../img/big-project-image.png"
+const card = () => {
+	return (
+        <>
+        <div className="row">
+            <div className="col-2"></div>
+            <div className="col-8 d-flex justify-content-between">
+
+
+
+            </div>
+            <div className="col-2"></div>
+        </div>
+        
+        </>
+    );
+};
