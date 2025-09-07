5274e0688f8374824a3b3000973367f584e2b1c0
Seeker90 <chris.carver90@icloud.com>
2025-09-07 21:47:30 +0000

    updated card.jsx file

--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 import cardImage from"../../img/big-project-image.png"
 
-const card = () => {
-	return (
+const Card = () => {
+	const cards = [
+        {   name:"Card1",
+            title: "Card Title", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            img: cardImage,
+        },
+         {  name:"Card2",
+            title: "Card Title", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            img: cardImage,
+        },
+         {  name:"Card3",
+            title: "Card Title", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            img: cardImage,
+        },
+         {  name:"Card4",
+            title: "Card Title", 
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            img: cardImage,
+        },
+    ];
+    
+    return (
         <>
             <div className="row">
-                <div className="col-2"></div>
-                <div className="col-8 d-flex justify-content-between">
-                    <div className="card" {{style=width: "18rem;"}}>
-                        <img src={cardImage} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis nisi quis sapien bibendum, at cursus nisi imperdiet. Fusce et nisl nec sem interdum fringilla. Nunc eu velit non augue bibendum venenatis.</p>
-                            <a href="#" className="btn btn-primary">Find out More!</a>
+                <div className="col-1"></div>
+                <div className="col-10 d-flex justify-content-between">
+                    {cards.map((card, index) => (
+                        <div className="card-img" style={{ width: "10rem"}} key={index}>
+                            <img src={card.img} className={card.name} alt={card.title} style={{width: "10rem"}}/>
+                            <div className="card-body">
+                                <h5>{card.title}</h5>
+                                <p>{card.text}</p>
+                                <a href="#" className="btn btn-primary">Find out More!</a>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-2"></div>
+                    ))}
+                  </div>
+                <div className="col-1"></div>
             </div>
-        
         </>
     );
 };
 
-export default card;
+export default Card;
